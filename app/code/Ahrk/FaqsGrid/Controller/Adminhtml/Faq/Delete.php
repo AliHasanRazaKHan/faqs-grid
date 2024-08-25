1a033831164ead18641bca8e0d91c258d7077183
AliHasanRazaKHan <alihsnrazakhan@gmail.com>
2024-08-25 23:18:52 +0500

    Applied "is_published" filter on page load

--- app/code/Ahrk/FaqsGrid/Controller/Adminhtml/Faq/Delete.php
+++ app/code/Ahrk/FaqsGrid/Controller/Adminhtml/Faq/Delete.php
@@ -2,15 +2,60 @@
 
 namespace Ahrk\FaqsGrid\Controller\Adminhtml\Faq;
 
+use Ahrk\FaqsGrid\Model\Faq;
 use Magento\Backend\App\Action;
+use Magento\Backend\App\Action\Context;
 use Magento\Framework\App\Action\HttpGetActionInterface;
+use Ahrk\FaqsGrid\Model\ResourceModel\Faq as FaqResource;
+use Ahrk\FaqsGrid\Model\FaqFactory;
+use Magento\Framework\Controller\Result\Redirect;
+use Magento\Framework\Controller\ResultFactory;
 
 class Delete extends Action implements HttpGetActionInterface
 {
 
-    const ADMIN_RESOURCE = 'Ahrk_faqsgrid::faq_delete';
-    public function execute()
-    {
+    protected FaqResource $faqResource;
 
+    protected FaqFactory $faqFactory;
+    const ADMIN_RESOURCE = 'Ahrk_faqsgrid::faq_delete';
+
+    /**
+     * @param Context $context
+     * @param FaqFactory $faqFactory
+     * @param FaqResource $faqResource
+     * @return void
+     */
+    public function __construct(
+        Context     $context,
+        FaqFactory  $faqFactory,
+        FaqResource $faqResource,
+    )
+    {
+        $this->faqFactory = $faqFactory;
+        $this->faqResource = $faqResource;
+        parent::__construct($context);
+    }
+
+    public function execute(): Redirect
+    {
+        try {
+            $faqId = $this->getRequest()->getParam('id');
+            /**
+             * @var Faq $faq
+             */
+            $faq = $this->faqFactory->create();
+
+            $this->faqResource->load($faq, $faqId);
+            if ($faq->getId()) {
+                $this->faqResource->delete($faq);
+                $this->messageManager->addSuccessMessage(__("The record has been deleted."));
+        } else {
+                $this->messageManager->addErrorMessage(__("The record does not exist."));
+            }
+        } catch (\Exception $e) {
+            $this->messageManager->addErrorMessage($e->getMessage());
+        }
+
+        return $this->resultFactory->create(ResultFactory::TYPE_REDIRECT)->setPath("*/*");
     }
 }
---
+++ app/code/Ahrk/FaqsGrid/Controller/Adminhtml/Faq/NewAction.php
@@ -0,0 +1,38 @@
+<?php declare(strict_types=1);
+
+namespace Ahrk\FaqsGrid\Controller\Adminhtml\Faq;
+
+use Magento\Backend\App\Action;
+use Magento\Backend\App\Action\Context;
+use Magento\Framework\App\Action\HttpGetActionInterface;
+use Magento\Framework\View\Result\PageFactory;
+use Magento\Framework\View\Result\Page;
+
+class NewAction extends Action implements HttpGetActionInterface
+{
+    const ADMIN_RESOURCE = "Ahrk_FaqsGrid::faq_save";
+
+    /**
+     *
+     * @param Context $context
+     * @param PageFactory $pageFactory
+     */
+    public function __construct(
+        Context $context,
+        private readonly PageFactory $pageFactory
+    )
+    {
+        parent::__construct($context);
+    }
+
+    /**
+     *
+     * @return Page
+     */
+    public function execute(): Page
+    {
+        $page = $this->pageFactory->create();
+        $page->getConfig()->getTitle()->prepend(__("New FAQ"));
+        return $page;
+    }
+}
